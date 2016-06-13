cc9789d7832f2fbbe3318ad1f9f1643f0e61ace6
ReyBT <rtianero@gmail.com>
2016-06-14 00:28:09 +0800

    Login and Product Entry

--- WarehouseInventory/Login/Login.xaml.cs
+++ WarehouseInventory/Login/Login.xaml.cs
@@ -5,9 +5,9 @@ namespace WarehouseInventory
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Login : Window
     {
-        public MainWindow()
+        public Login()
         {
             InitializeComponent();
         }
